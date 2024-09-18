c7079f21e926a045fcd9345359a073cd13d43e58
Akash Gupta <akashgupta01082002@gmail.com>
2024-09-18 17:46:18 +0530

    Add files via upload

---
+++ Web.js
@@ -0,0 +1,58 @@
+let Home = document.querySelector(".home");
+let skill = document.querySelector(".Skills");
+let project = document.querySelector("Project");
+let Experience = document.querySelector(".Experience");
+let contact = document.querySelector(".contact");
+
+
+contact = document.addEventListener(onclick, function) {
+    Home = window.console(".footer");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let project1 = document.querySelector("textbyimg")
+{
+    project1.style.hover
+}
+
+
+
+// gsap.to( "#container1",{
+//     scrollTrigger: {
+//         trigger : #container1 ,
+//     start: top top,
+//     end: bottom top,
+//     markers: true,
+//     pin: true,
+//     scroller: #main
+// }   
+// } )
+
+
+
+
+
+
+
+
+
+
+
+
+
